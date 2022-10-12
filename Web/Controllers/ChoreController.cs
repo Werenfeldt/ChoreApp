860508c14729b23a413b82a0935bda0d8cf17f43
Werenfeldt <josefine.werenfeldt@hotmail.com>
2022-10-12 14:58:32 +0200

    Added Chore controller. Need to fix tests

---
+++ Web/Controllers/ChoreController.cs
@@ -0,0 +1,57 @@
+namespace Web.Controllers;
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class ProjectsController : ControllerBase
+{
+    public Func<string> GetObjectId;
+    private IChoreRepository _choreRepository;
+
+    public ProjectsController(IChoreRepository repo)
+    {
+        _choreRepository = repo;
+        GetObjectId = () =>
+            User.GetObjectId() == null ? "1" : User.GetObjectId();
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<ChoreDTO>> Get()
+    {
+        return await _choreRepository.ReadAllChoresAsync(Guid.Parse("30bc356c-f2cf-42b6-961e-dfd178a50a66"));
+    }
+
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ChoreDTO), StatusCodes.Status200OK)]
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ChoreDTO>> Get(Guid id)
+        => (await _choreRepository.ReadChoreByIdAsync(id)).ToActionResult();
+
+    [HttpPost]
+    [Authorize(Roles = "Supervisor")]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ChoreDTO), StatusCodes.Status201Created)]
+    public async Task<IActionResult> Post(CreateChoreDTO chore)
+    {
+        //chore.CreatedByUserId = Guid.Parse(GetObjectId());
+
+        var created = await _choreRepository.CreateChoreAsync(chore);
+
+        return CreatedAtAction(nameof(Get), new { created.Id }, created);
+    }
+
+    [HttpPut("{id}")]
+    [Authorize(Roles = "Supervisor")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Put(Guid id, [FromBody] UpdateChoreDTO chore)
+        => (await _choreRepository.EditChoreAsync(id, chore)).ToActionResult();
+
+    [HttpDelete("{id}")]
+    [Authorize(Roles = "Supervisor")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Delete(Guid id)
+        => (await _choreRepository.DeleteChoreByIdAsync(id)).ToActionResult();
+}
---
+++ Web/Model/Extentions.cs
@@ -0,0 +1,17 @@
+
+namespace Web.Model;
+
+
+public static class Extensions
+{
+    public static IActionResult ToActionResult(this Response response) => response switch
+    {
+        Updated => new NoContentResult(),
+        Deleted => new NoContentResult(),
+        NotFound => new NotFoundResult(),
+        _ => throw new NotSupportedException($"{response} not supported")
+    };
+
+    public static ActionResult<T> ToActionResult<T>(this Option<T> option) where T : class
+        => option.IsSome ? option.Value : new NotFoundResult();
+}
